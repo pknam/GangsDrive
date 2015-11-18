f80a64e3000e52f6e1f0c2dfc2ffa20c520dbf63
snowNymph <hong921122@nate.com>
2015-11-18 23:27:55 +0900

    mount changed event added

---
+++ GangsDrive/connector/MountChangedArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GangsDrive.connector
+{
+    class MountChangedArgs : EventArgs
+    {
+        private bool _mountState;
+
+        public bool MountState
+        {
+            get
+            {
+                return _mountState;
+            }
+        }
+
+        public MountChangedArgs(bool mountState)
+        {
+            _mountState = mountState;
+        }
+    }
+}
