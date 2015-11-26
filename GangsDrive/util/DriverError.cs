a8585cfb83f3a6505086bc70f99c7ba410d71fd2
SeokJun Hong <hong921122@gmail.com>
2015-11-26 13:00:09 +0900

    Add util method for handling exception messages

---
+++ GangsDrive/util/DriverError.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GangsDrive.util
+{
+    class DriverError
+    {
+        /// <summary>
+        /// util method for debugging message
+        /// </summary>
+        /// <param name="ex">exception object</param>
+        /// <param name="context">driver name</param>
+        /// <param name="mount_state">mount state</param>
+        public static void DebugError(Exception ex, string context, bool mount_state)
+        {
+            System.Windows.Forms.MessageBox.Show(ex.StackTrace);
+        }
+    }
+}
